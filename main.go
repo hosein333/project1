dedcb80cb0c365bfca89059515e0f30909dc2307
mohammadjavad <hossinbabayy1382@gmail.com>
2023-12-05 18:58:31 +0330

    add motoghayer var4 and var5 project1

--- main.go
+++ main.go
@@ -7,8 +7,12 @@ func main() {
 	var (
 		var1 = -1.2
 		var2 = 1
-		var3 = false
+		var3 bool = false
+		var4 string 
 	)
-	fmt.Println(var1, var2, var3)
+	var4 = "empty string"
+	var5 := "another string"
+
+	fmt.Println(var1, var2, var3, var4, var5)
 }
 
