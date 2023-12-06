cdad2397718c6c2bd05c7ee444c9fdf0333b06fb
mohammadjavad <hossinbabayy1382@gmail.com>
2023-12-06 23:07:38 +0330

    var g == var f project1

--- main.go
+++ main.go
@@ -1,26 +1,11 @@
 package main
 
-import (
-	"fmt"
-        "math"
-)
-
-const float64EqualityThreshold = 1e-9
+import "fmt"
+type float float64
 
 func main() {
+	var f float = 5.2
+	var g float64 = 5.2
 
-
-
-	var (
-		var1 float64 = -1.2
-		var2 int = 1
-	)
-	var3 := var1 + float64(var2)
-	
-	if math.Abs(var3-(-0.2)) < float64EqualityThreshold {
-		fmt.Println("It is Equal")
-	}
-
-	fmt.Println(var1, var2, var3,)
+	fmt.Println("f == g", f == float(g))
 }
-
